64021efb519f0fc3a0346a0903d8eca41352ec0f
tchiinhemba <tchiinhemba@gmail.com>
2025-07-07 12:27:29 +0100

    change the wrapper className name of register component

--- src/screens/register/register.jsx
+++ src/screens/register/register.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Register() {
   return (
     <React.Fragment>
-      <div className="wi_wrapper"></div>
+      <div className="re_wrapper"></div>
     </React.Fragment>
   );
 }
